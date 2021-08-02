2395913f735459b1f0f0f0b91f208b8de3d68b76
root <root@superserver>
2021-08-02 11:43:16 -0500

    [all proyect] copia de seguridad

---
+++ odoo/lsv_project/__manifest__.py
@@ -0,0 +1,11 @@
+{
+	'name': "MyFirtModule",
+	'version': "10-devl10.0",
+	'summary': "This is my firt module, make in odoo, I'm happy (Y)",
+	'depends': ['project', 'contacts'],
+	'data': {},
+	'installable': True,
+	'auto_install': False,
+	'license': 'LGPL-3',
+
+}
